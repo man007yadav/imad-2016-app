d7997f93293efd033e7b6fbe4f86de00886ecaec
Manoj Yadav <man007yadav@gmail.com>
2016-09-23 20:50:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,23 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne= {
-  title: "Manoj Yadav | Article one",
-  heading : "Article one",
-  date : "Sept 23,2016",
-  content: `<p>
-                This is paragraph one, which is nothing which useful for me or you.
-            </p> 
-            
-            <p>
-                This is paragraph two, may be useful for my love.
-            </p>
-            
-            <p>
-                This paragraph three, and cant help making this page at all.
-            </p>
-             `
-  
+var articles = {
+
+    'article-one' :{
+      title: "Manoj Yadav | Article one",
+      heading : "Article one",
+      date : "Sept 23,2016",
+      content: `<p>
+                    This is paragraph one, which is nothing which useful for me or you.
+                </p> 
+                
+                <p>
+                    This is paragraph two, may be useful for my love.
+                </p>
+                
+                <p>
+                    This paragraph three, and cant help making this page at all.
+                </p>
+                 `
+    },
+    
+    'article-two' :{
+      title: "Manoj Yadav | Article two",
+      heading : "Article two",
+      date : "Sept 23,2016",
+      content: `<p>
+                    This is paragraph one, which is nothing which useful for me or you.
+                </p> 
+                
+                <p>
+                    This is paragraph two, may be useful for my love.
+                </p>
+                
+                <p>
+                    This paragraph three, and cant help making this page at all.
+                </p>
+                 `
+    },
+    
+    'article-three' :{
+      title: "Manoj Yadav | Article three",
+      heading : "Article three",
+      date : "Sept 23,2016",
+      content: `<p>
+                    This is paragraph one, which is nothing which useful for me or you.
+                </p> 
+                
+                <p>
+                    This is paragraph two, may be useful for my love.
+                </p>
+                
+                <p>
+                    This paragraph three, and cant help making this page at all.
+                </p>
+                 `
+    }
   
     
 };
@@ -85,20 +123,24 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function (req,res) {
-   res.send(createTemplate(articleOne)); 
+app.get('/:articleName',function (req,res) {
+    //articleName==article-one
+    //article[articleName]==content {} article one
+    var articleName=request.params.articleName;
+   res.send(createTemplate(articleName[articleName])); 
 });
 
-app.get('/article-two',function (req,res) {
-   res.sendFile(path.join(__dirname,'ui','article-two.html')); 
-});
-app.get('/article-three',function (req,res) {
-   res.sendFile(path.join(__dirname,'ui','article-three.html')); 
-});
+//<!--app.get('/article-two',function (req,res) {
+  // res.sendFile(path.join(__dirname,'ui','article-two.html')); 
+    
+//});
+//app.get('/article-three',function (req,res) {
+ //  res.sendFile(path.join(__dirname,'ui','article-three.html')); 
+//});//-->
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+//app.get('/ui/style.css', function (req, res) {
+  //res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+//});
 
 app.get('/ui/images.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'images.jpg'));
