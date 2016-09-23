e22c884e434220e6eb24858f7eb1ef9c0208574b
Manoj Yadav <man007yadav@gmail.com>
2016-09-23 18:26:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,10 @@ app.get('/article-one',function (req,res) {
 });
 
 app.get('/article-two',function (req,res) {
-   res.send('Article two is called and will shown here'); 
-});
+   res.sendFile(path.join(__dirname,'ui','article-two.html')); 
 
 app.get('/article-three',function (req,res) {
-   res.send('Article three is called and will shown here'); 
+   res.sendFile(path.join(__dirname,'ui','article-three.html')); 
 });
 
 app.get('/ui/style.css', function (req, res) {
