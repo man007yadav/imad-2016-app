3d2800dea7575c2ca60879578c30bad20c565fe6
Manoj Yadav <man007yadav@gmail.com>
2016-09-24 00:33:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,18 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = "We are akatsuki";
+
+var img = document.getElementById('img');
+var marginleft=0;
+
+function moveRight(){
+   marginleft = marginleft + 5;
+   img.style.marginLeft = marginleft + "px"; 
+    
+}
+
+img.onclick = function(){
+    
+    var interval = setInterval(moveRight,50);
+    //img.style.marginLeft= '100px';
+}
