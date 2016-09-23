9ae90b0dea1e622ee135386704eeff8fd672ef98
Manoj Yadav <man007yadav@gmail.com>
2016-09-24 01:36:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,13 @@ img.onclick = function(){
 
 var button = document.getElementById('counter');
 var counter = 0;
+
+
+
 button.onclick = function(){
+    
+    //Render the variable in correct span
+    var span = document.getElementById('count');
     counter = counter + 1 ;
     span.innerHTML = counter.toString();
 }
