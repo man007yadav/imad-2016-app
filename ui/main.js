771230943cac712754202e18f6a2a989e198371f
Manoj Yadav <man007yadav@gmail.com>
2016-09-24 01:32:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,11 @@ img.onclick = function(){
     var interval = setInterval(moveRight,50);
     //img.style.marginLeft= '100px';
 }
+
+
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    counter = counter + 1 ;
+    span.innerHTML = counter.toString();
+}
