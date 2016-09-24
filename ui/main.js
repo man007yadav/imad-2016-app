768afadb4b20d55425e1653edd869d7759a81ffe
Manoj Yadav <man007yadav@gmail.com>
2016-09-24 09:57:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -45,7 +45,7 @@ button.onclick = function(){
     };
     
     //Make the request
-    request.open('GET',"http://man007yadav.imad.hasura-app.io/counter",true);
+    request.open('GET','http://man007yadav.imad.hasura-app.io/counter',true);
     request.send(null);
     
 };
